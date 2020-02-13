9a1b74c9aeded4f5d448882115149f329b522ee7
Louis-yes <luegraham@gmail.com>
2020-02-13 20:28:55 +1100

    Compiled preact photoblog first commit, in all its mess

---
+++ precache-manifest.d3de596ca40fe60e8e130b3c4281ee2b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b59a0b2c290bb965cd976462315d6d",
+    "url": "/assets/favicon.png"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "5b48a699620e29ca274c",
+    "url": "/bundle.5b48a.js"
+  },
+  {
+    "revision": "5b48a699620e29ca274c",
+    "url": "/bundle.fb277.css"
+  },
+  {
+    "revision": "4a3150b029b918206181810252edc07c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea3a1def33ca0b225ddf",
+    "url": "/polyfills.ea3a1.js"
+  },
+  {
+    "revision": "04f2fb7b35b1a6a260ae",
+    "url": "/route-home.chunk.04f2f.js"
+  },
+  {
+    "revision": "04f2fb7b35b1a6a260ae",
+    "url": "/route-home.chunk.3f3a8.css"
+  },
+  {
+    "revision": "16ba55973463686afbf6",
+    "url": "/route-profile.chunk.16ba5.js"
+  },
+  {
+    "revision": "16ba55973463686afbf6",
+    "url": "/route-profile.chunk.62c75.css"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "3aa16d95f2210c339e89682f6331d2cb",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
